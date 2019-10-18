0bafdc7bbf547a2fa57b3cf9efa977100fdc1547
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-18 12:44:00 +0300

    прокинул динамические данные в компонент приветствия из точки входа

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,7 +1,12 @@
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 
-const App = () => {
-  return <WelcomeScreen />;
+const App = (props) => {
+  const {gameTime, errorCount} = props;
+
+  return <WelcomeScreen
+    time={gameTime}
+    errorCount={errorCount}
+  />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,16 @@ import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
 const init = () => {
+  const settings = {
+    gameTime: 5,
+    errorCount: 3,
+  };
+
   ReactDOM.render(
-      <App/>,
+      <App
+        gameTime={settings.gameTime}
+        errorCount={settings.errorCount}
+      />,
       document.getElementById(`root`)
   );
 };
