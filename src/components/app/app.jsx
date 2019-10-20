a4f5e64923b17b5f449b262a3d0bfd9288283dcf
Funky-Juice <isa86box@gmail.com>
2019-10-20 15:32:02 +0300

    welcome-srceen: добавил обработчик клика и прокинул в него метод

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,17 +2,19 @@ import PropTypes from 'prop-types';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 
 const App = (props) => {
-  const {gameTime, errorCount} = props;
+  const {gameTime, errorCount, gameStart} = props;
 
   return <WelcomeScreen
     time={gameTime}
     errorCount={errorCount}
+    start={gameStart}
   />;
 };
 
 App.propTypes = {
   gameTime: PropTypes.number.isRequired,
-  errorCount: PropTypes.number.isRequired
+  errorCount: PropTypes.number.isRequired,
+  gameStart: PropTypes.func
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import App from './components/app/app';
 const init = () => {
   const settings = {
     gameTime: 5,
-    errorCount: 3
+    errorCount: 3,
+    gameStart: () => {
+      return `start game!`;
+    }
   };
 
   ReactDOM.render(
       <App
         gameTime={settings.gameTime}
         errorCount={settings.errorCount}
+        gameStart={settings.gameStart}
       />,
       document.getElementById(`root`)
   );
