4bead7371f5f8663de84c16fc212b30e5797df7f
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-10-25 11:55:18 +0300

    обновил старые тесты

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -3,7 +3,7 @@ import App from './app';
 
 it(`App correctly renders after relaunch`, () => {
   const tree = renderer
-    .create(<App gameTime={0} errorCount={0} gameStart={() => {}}/>)
+    .create(<App questions={[]} gameTime={0} errorCount={0}/>)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
