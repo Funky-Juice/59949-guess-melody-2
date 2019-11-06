b5bffeeef56db0cdfa323a32d1d9b1d2be6715a7
Funky-Juice <isa86box@gmail.com>
2019-11-06 14:13:49 +0300

    добавил в проект redux; создал и подключил store;

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store/store';
 import App from './components/app/app';
 import {questions, params} from './mocks/questions';
 
 const init = (gameQuestions, gameParams) => {
 
-  ReactDOM.render(
-      <App
-        questions={gameQuestions}
-        gameTime={gameParams.gameTime}
-        errorCount={gameParams.errorCount}
-      />,
-      document.getElementById(`root`)
+  ReactDOM.render(<Provider store={store}>
+    <App
+      questions={gameQuestions}
+      gameTime={gameParams.gameTime}
+      errorCount={gameParams.errorCount}
+    />
+  </Provider>,
+  document.getElementById(`root`)
   );
 };
 
