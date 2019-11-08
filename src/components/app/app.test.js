3c95bcff3733473031edf446f4c79018a8ba6251
Иншаков Сергей <inshakovsa@edu.mos.ru>
2019-11-08 18:23:14 +0300

    поправил тест для App

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,9 +1,18 @@
 import renderer from 'react-test-renderer';
-import App from './app';
+import {App} from './app';
 
 it(`App correctly renders after relaunch`, () => {
   const tree = renderer
-    .create(<App questions={[]} gameTime={0} maxMistakes={0}/>)
+    .create(<App
+      level={-1}
+      questions={[{}]}
+      gameTime={0}
+      mistakes={0}
+      maxMistakes={0}
+      onGameEnd={() => {}}
+      onGameStart={() => {}}
+      onUserAnswer={() => {}}
+    />)
     .toJSON();
 
   expect(tree).toMatchSnapshot();
